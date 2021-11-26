868e4580e6de45aea44af3b1e98373f54e366c5e
tangkhanhchuong <chuongtangkhanh2104@gmail.com>
2021-11-26 09:29:15 +0700

    fix editing  profile picture and avatar

--- server/schemas/user_schema.js
+++ server/schemas/user_schema.js
@@ -4,38 +4,38 @@ const Schema = mongoose.Schema
 const findOrCreate = require('mongoose-findorcreate')
 
 const UserSchema = new Schema({
-    username: { type: String, require, trim: true },
-    email: { type: String, trim: true, unique: true },
-    password: { type: String },
-    bio: { type: String },
-    location: { type: String },
-    website: { type: String },
-    joinedDate: { type: Date, default: new Date(1, 1, 2020) },
-    avatar: {type: String, default: ''},
-    coverPicture: { type: String, default: '' },
-    authMethod: { type: String, default: 'JWT' },
-    fbId: { type: String }
+  username: { type: String, require, trim: true },
+  email: { type: String, trim: true, unique: true },
+  password: { type: String, default: '' },
+  bio: { type: String, default: '' },
+  location: { type: String, default: '' },
+  website: { type: String, default: '' },
+  joinedDate: { type: Date, default: new Date(1, 1, 2020) },
+  avatar: { type: String, default: '' },
+  coverPicture: { type: String, default: '' },
+  authMethod: { type: String, default: 'JWT' },
+  fbId: { type: String }
 }, { timestamps: true })
 
 UserSchema.plugin(findOrCreate)
 
 UserSchema.pre(
-    'save',
-    async function(next){
-        const { authMethod, password } = this
-        if( authMethod !== 'JWT')   next()
+  'save',
+  async function (next) {
+    const { authMethod, password } = this
+    if (authMethod !== 'JWT') next()
 
-        const hash = await bcrypt.hash(password, 10)
-        this.password = hash
-        next()
-    }
+    const hash = await bcrypt.hash(password, 10)
+    this.password = hash
+    next()
+  }
 )
 
-UserSchema.methods.isValidPassword = async function(password) {
-    const user = this
-    if(user.authMethod !== 'JWT')   return true
-    const compare = await bcrypt.compare(password, user.password)
-    return compare
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this
+  if (user.authMethod !== 'JWT') return true
+  const compare = await bcrypt.compare(password, user.password)
+  return compare
 }
 
 const User = mongoose.model('User', UserSchema)
